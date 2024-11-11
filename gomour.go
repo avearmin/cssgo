3e378e05d03130b27cc0273dc6855c798129136d
avearmin <arminradoncic1@gmail.com>
2024-11-11 14:44:07 -0500

    Remove statefulWriter

--- gomour.go
+++ gomour.go
@@ -7,15 +7,3 @@ import (
 type Node interface {
 	Render(w io.Writer) error
 }
-
-type statefulWriter struct {
-	w   io.Writer
-	err error
-}
-
-func (w *statefulWriter) Write(p []byte) {
-	if w.err != nil {
-		return
-	}
-	_, w.err = w.w.Write(p)
-}
