ef1297f09ff4a0822a80131be54699100319eaea
avearmin <arminradoncic1@gmail.com>
2024-11-16 17:47:51 -0500

    Move all color related values to color_value.go

---
+++ color_value.go
@@ -0,0 +1,112 @@
+package gomour
+
+import (
+	"fmt"
+	"io"
+	"strings"
+)
+
+type ColorValue interface {
+	ValueNode
+	colorValue()
+}
+
+type ColorValueFunc func(io.Writer) error
+
+func (f ColorValueFunc) RenderCSS(w io.Writer) error {
+	return f(w)
+}
+
+func (f ColorValueFunc) String() string {
+	var b strings.Builder
+	_ = f.RenderCSS(&b)
+	return b.String()
+}
+
+func (f ColorValueFunc) valueNode()  {}
+func (f ColorValueFunc) colorValue() {}
+
+func Hex(hexdec int) ColorValueFunc {
+	return ColorValueFunc(func(w io.Writer) error {
+		hexStr := fmt.Sprintf("#%06x", hexdec)
+		_, err := w.Write([]byte(hexStr))
+		return err
+	})
+}
+
+func RGB(r, g, b int) ColorValueFunc {
+	return ColorValueFunc(func(w io.Writer) error {
+		rgbStr := fmt.Sprintf("rgb(%d, %d, %d)", r, g, b)
+		_, err := w.Write([]byte(rgbStr))
+		return err
+	})
+}
+
+func RGBA(r, g, b int, alpha float64) ColorValueFunc {
+	return ColorValueFunc(func(w io.Writer) error {
+		rgbaStr := fmt.Sprintf("rgba(%d, %d, %d, %f)", r, g, b, alpha)
+		_, err := w.Write([]byte(rgbaStr))
+		return err
+	})
+}
+
+func Color(color string) ColorValueFunc {
+	return ColorValueFunc(func(w io.Writer) error {
+		_, err := w.Write([]byte(color))
+		return err
+	})
+}
+
+func HSL(hue uint, saturation, lightness float64) ColorValueFunc {
+	if hue > 360 {
+		hue = 360
+	}
+	if saturation < 0.0 {
+		saturation = 0.0
+	}
+	if saturation > 1.0 {
+		saturation = 1.0
+	}
+	if lightness < 0.0 {
+		lightness = 0.0
+	}
+	if lightness > 1.0 {
+		lightness = 1.0
+	}
+
+	return ColorValueFunc(func(w io.Writer) error {
+		hslStr := fmt.Sprintf("hsl(%d, %.0f%%, %.0f%%)", hue, saturation*100, lightness*100)
+		_, err := w.Write([]byte(hslStr))
+		return err
+	})
+}
+
+func HSLA(hue uint, saturation, lightness, alpha float64) ColorValueFunc {
+	if hue > 360 {
+		hue = 360
+	}
+	if saturation < 0.0 {
+		saturation = 0.0
+	}
+	if saturation > 1.0 {
+		saturation = 1.0
+	}
+	if lightness < 0.0 {
+		lightness = 0.0
+	}
+	if lightness > 1.0 {
+		lightness = 1.0
+	}
+	if alpha < 0.0 {
+		alpha = 0.0
+	}
+	if alpha > 1.0 {
+		alpha = 1.0
+	}
+
+	return ColorValueFunc(func(w io.Writer) error {
+		hslaStr := fmt.Sprintf("hsla(%d, %.0f%%, %.0f%%, %.1f)", hue, saturation*100, lightness*100, alpha)
+		_, err := w.Write([]byte(hslaStr))
+		return err
+	})
+}
