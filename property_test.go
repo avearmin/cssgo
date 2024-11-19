e5b675298699ac08f86d59304c6791a88a4df05e
avearmin <arminradoncic1@gmail.com>
2024-11-19 16:13:21 -0500

    Refactor property_test
    The logic for running tests was encapsulated into a function to create easier test tables for other test functions.

--- property_test.go
+++ property_test.go
@@ -1,41 +1,50 @@
 package gomour
 
-import "testing"
+import (
+	"strings"
+	"testing"
+)
+
+type test struct {
+	name  string
+	input Node
+	want  string
+}
+
+func RunTests(t *testing.T, tests ...test) {
+	for _, te := range tests {
+		func(t2 *testing.T) {
+			w := strings.Builder{}
+			te.input.RenderCSS(&w)
+			got := w.String()
+			if got != te.want {
+				t2.Fatalf("TESTCASE %s: FAIL\ngot: %s != want: %s", te.name, got, te.want)
+			}
+		}(t)
+	}
+}
 
 func TestPadding(t *testing.T) {
-	tests := []struct {
-		name  string
-		input PropertyNodeFunc
-		want  string
-	}{
-		{
+	RunTests(t,
+		test{
 			"padding with 1 value",
 			Padding(PX(10), nil, nil, nil),
 			"padding: 10px;",
 		},
-		{
+		test{
 			"padding with 2 values",
 			Padding(PX(10), PX(20), nil, nil),
 			"padding: 10px 20px;",
 		},
-		{
+		test{
 			"padding with 3 values",
 			Padding(PX(10), PX(20), PX(30), nil),
 			"padding: 10px 20px 30px;",
 		},
-		{
-			"padding with 3 values",
+		test{
+			"padding with 4 values",
 			Padding(PX(10), PX(20), PX(30), PX(40)),
 			"padding: 10px 20px 30px 40px;",
 		},
-	}
-
-	for _, test := range tests {
-		func(t2 *testing.T) {
-			got := test.input.String()
-			if got != test.want {
-				t2.Fatalf("TESTCASE %s: FAIL\ngot: %s != want: %s", test.name, got, test.want)
-			}
-		}(t)
-	}
+	)
 }
