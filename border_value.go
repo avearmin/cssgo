400c95ea3f5062ea9e898ae7187a350be878b576
avearmin <arminradoncic1@gmail.com>
2024-11-27 19:34:05 -0500

    Add documentation for border_value.go

--- border_value.go
+++ border_value.go
@@ -2,13 +2,18 @@ package gomour
 
 import "io"
 
+// BorderStyleValue defines an interface for CSS-compatible border style values.
+// This ensures that any type implementing this interface can be rendered as a valid CSS border style.
 type BorderStyleValue interface {
 	ValueNode
 	borderStyleValue()
 }
 
+// BorderStyle represents a CSS border style, such as "solid", "dotted", or "dashed".
+// It is a concrete type that implements the BorderStyleValue interface.
 type BorderStyle string
 
+// Predefined border style values as per the CSS specification.
 const (
 	Solid  BorderStyle = "solid"
 	Dotted BorderStyle = "dotted"
