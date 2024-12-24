dd1fd7ca17edcdea5b1fc50476b0299b4aabd4da
avearmin <arminradoncic1@gmail.com>
2024-12-23 21:49:03 -0500

    Add comments to flex_direction_value.go

--- flex_direction_value.go
+++ flex_direction_value.go
@@ -2,11 +2,15 @@ package cssgo
 
 import "io"
 
+// FlexDirectionValue defines an interface for CSS-compatible flex direction values.
+// This ensures that any type implementing this interface can be rendered as a valid CSS flex direction.
 type FlexDirectionValue interface {
 	ValueNode
 	flexDirectionValue()
 }
 
+// FlexDirectionType represents a CSS flex direction, such as "row", "row-reverse", or "column".
+// It is a concrete type that implements the FlexDirectionValue interface.
 type FlexDirectionType string
 
 func (f FlexDirectionType) RenderCSS(w io.Writer) error {
@@ -17,6 +21,7 @@ func (f FlexDirectionType) RenderCSS(w io.Writer) error {
 func (f FlexDirectionType) valueNode()        {}
 func (f FlexDirectionType) borderStyleValue() {}
 
+// Predefined flex direction values as per the CSS specification.
 const (
 	Row           FlexDirectionType = "row"
 	RowReverse    FlexDirectionType = "row-reverse"
